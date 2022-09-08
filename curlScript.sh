a29189ec9963738100c5fddbedc48b41975a2b2b
Djordje Petrovic <djordje.petrovic@htecgroup.com>
2022-09-08 17:12:27 +0200

    version 1.1

--- curlScript.sh
+++ curlScript.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -l
 
-NBN=$(cat /var/lib/docker/volumes/DockerJenkins/_data/jobs/test3/branches/feature-pr-branch/nextBuildNumber)
+NBN=$(sudo cat /var/lib/docker/volumes/DockerJenkins/_data/jobs/test3/branches/feature-pr-branch/nextBuildNumber)
 echo "Next build number will be $NBN"
 #curl -u htec11090:11aa5d3fd3133ee42af9e5c2df158efb09 "http://172.18.94.170:8080/job/test3/job/feature-pr-branch/9/logText/progressiveText?start=0"
