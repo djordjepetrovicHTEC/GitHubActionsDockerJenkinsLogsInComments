ae31bef0984e9e57d74cdccba27a459ab7c84a5a
Djordje Petrovic <djordje.petrovic@htecgroup.com>
2022-09-09 14:23:40 +0200

    version 3.4

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
-cat PRCommentLog.txt
-body=$(cat PRCommentLog.txt | sed 's/^ [ \t]*//;s/[ \t]*$//')
+#cat PRCommentLog.txt
+body=$(cat PRCommentLog.txt)
+#| sed 's/^ [ \t]*//;s/[ \t]*$//')
 echo $body
 #body=$(cat PRCommentLog.txt)
 echo ::set-output name=body::$body
