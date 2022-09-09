d10232483191a252827db77a315ef1328529c8cd
Djordje Petrovic <djordje.petrovic@htecgroup.com>
2022-09-09 15:00:57 +0200

    test0.2

--- script.sh
+++ script.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -l
 body=$(cat finishedFile.txt)
+echo "---------------"
 #| sed 's/^ [ \t]*//;s/[ \t]*$//')
+echo $body
+echo "---------------"
+#body=$(cat PRCommentLog.txt)
 echo ::set-output name=body::$body
