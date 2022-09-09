0e418a038613cc3fec09332e503876ae487c4b92
Djordje Petrovic <djordje.petrovic@htecgroup.com>
2022-09-09 13:17:01 +0200

    version 2.5

--- curlScript.sh
+++ curlScript.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
-exec > /home/htec11090/actions-runner2/_work/GitHubActionsDockerJenkinsLogsInComments/GitHubActionsDockerJenkinsLogsInComments/PRCommentLog.txt | sed "s/\\n/<br\/>/"
+exec $VAR > /home/htec11090/actions-runner2/_work/GitHubActionsDockerJenkinsLogsInComments/GitHubActionsDockerJenkinsLogsInComments/PRCommentLog.txt | sed "s/\\n/<br\/>/"
 # 2>&1
 
 NBN=$(sudo cat /var/lib/docker/volumes/DockerJenkins/_data/jobs/test3/branches/feature-pr-branch/nextBuildNumber)
 #echo "Next build number will be $NBN"
 NBN=$(( $NBN -1 ))
-curl -u htec11090:11aa5d3fd3133ee42af9e5c2df158efb09 "http://172.18.94.170:8080/job/test3/job/feature-pr-branch/$NBN/logText/progressiveText?start=361"
-sed 's/^ >[ \t]*//;s/[ \t]*$//'
+VAR=$(curl -u htec11090:11aa5d3fd3133ee42af9e5c2df158efb09 "http://172.18.94.170:8080/job/test3/job/feature-pr-branch/$NBN/logText/progressiveText?start=361" | sed 's/^ >[ \t]*//;s/[ \t]*$//')
+
 
